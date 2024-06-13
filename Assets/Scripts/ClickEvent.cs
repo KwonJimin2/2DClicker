ac421d7e9245eb62f4aa035bd4ca612bb3ea0e7c
KwonJimin2 <dnleogksrkddkwl@naver.com>
2024-06-13 14:04:33 +0900

    +ClickEvent

---
+++ Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickEvent : MonoBehaviour
+{
+    public Button Button;
+ 
+
+    public void OnClickButton()
+    {
+        Debug.Log("click");
+    }
+}
